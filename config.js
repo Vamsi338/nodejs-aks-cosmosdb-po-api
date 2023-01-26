dca42964dae214ec1df9986785efb5005a7eee1f
Vamsi338 <vamsikrishnajuttuka.97@gmail.com>
2023-01-26 16:39:22 +0000

    adding variables of cosmo db variables

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 let config = {}
 
-config.host = process.env.HOST || "Substitute the value of URI here";
-config.authKey = process.env.AUTH_KEY || "Substitute the value of PRIMARY KEY here";
+config.host = https://vamsikrishna.documents.azure.com:443/  || "Substitute the value of URI here";
+config.authKey = ZrtwWT4Cp0Wg0M8vLLfJftrmh6A4eDLFw5cyfGpDttdqS9cnCZiALp9A0mjnD7bcTwl4AuOj2O4fACDb1rx22Q== || "Substitute the value of PRIMARY KEY here";
 config.databaseId = "OrderDB";
 config.collectionId = "PurchaseOrders";
 
