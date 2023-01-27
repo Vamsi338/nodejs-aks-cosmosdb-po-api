d553ef689637babb447bd7f1ec648ef08e2ee723
Vamsi Krishna Juttuka <89961359+Vamsi338@users.noreply.github.com>
2023-01-27 13:34:03 +0530

    Update config.js
    removing / at the end

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 let config = {}
 
-config.host = https://vamsikrishna.documents.azure.com:443/  || "Substitute the value of URI here";
+config.host = https://vamsikrishna.documents.azure.com:443  || "Substitute the value of URI here";
 config.authKey = ZrtwWT4Cp0Wg0M8vLLfJftrmh6A4eDLFw5cyfGpDttdqS9cnCZiALp9A0mjnD7bcTwl4AuOj2O4fACDb1rx22Q== || "Substitute the value of PRIMARY KEY here";
 config.databaseId = "OrderDB";
 config.collectionId = "PurchaseOrders";
